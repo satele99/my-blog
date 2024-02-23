90475f68f4b409def89dbe60c1f9a2e72dffe0ae
satele99 <satele.works@gmail.com>
2024-02-23 11:28:04 -0500

    more css

--- js/main.js
+++ js/main.js
@@ -14,3 +14,74 @@ const toggleNav = () => navbar.classList.toggle('active');
 
 
 addEventOnElements(navTogglers, 'click', toggleNav);
+
+// when scrolled down to 100px header is active
+const header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+});
+
+/**
+ * SLIDER
+*/
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+
+/**
+ * NEXT SLIDE
+*/
+
+const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+  
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+  
+    moveSliderItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+  
+/**
+   * PREVIOUS SLIDE
+*/
+  
+const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+      currentSlidePos = totalSlidableItems;
+    } else {
+      currentSlidePos--;
+    }
+  
+    moveSliderItem();
+}
+  
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+  
+    moveSliderItem();
+});
+
