cbc0bfdcfbbf1bd3d22a9a689dabc0f6058fd673
satele99 <satele.works@gmail.com>
2024-02-25 06:51:13 -0500

    trying to get rid of gradient spot

--- js/main.js
+++ js/main.js
@@ -17,12 +17,15 @@ addEventOnElements(navTogglers, 'click', toggleNav);
 
 // when scrolled down to 100px header is active
 const header = document.querySelector('[data-header]');
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 100) {
         header.classList.add('active');
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove('active');
+        backTopBtn.classList.remove("active");
     }
 });
 
